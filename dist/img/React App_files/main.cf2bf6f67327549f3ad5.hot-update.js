50f3c476331386333bedaaebc1e00ff303233977
Chi Nguyen <nguyen.chi.byu@gmail.com>
2019-04-14 23:05:58 -0700

    updated trading_deployed link

---
+++ dist/img/React App_files/main.cf2bf6f67327549f3ad5.hot-update.js
@@ -0,0 +1,24 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/css/Signup.css":
+/*!*******************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--6-oneOf-3-1!./node_modules/postcss-loader/src??postcss!./src/css/Signup.css ***!
+  \*******************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var escape = __webpack_require__(/*! ../../node_modules/css-loader/lib/url/escape.js */ "./node_modules/css-loader/lib/url/escape.js");
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".wrapper {\n  background-image: url(" + escape(__webpack_require__(/*! ../img/bull.jpg */ "./src/img/bull.jpg")) + ");\n  background-size: cover;\n  background-repeat: no-repeat;\n  background-position: center;\n  min-height: 1100px;\n  min-height: 100vh;\n  color: white;\n}\n\n.inner{\n  margin: 0 auto;\n  padding: 130px;\n  width: 80%;\n  color: white;\n  min-height: 800px;\n}\n\n.log-form {\n  margin: 50px auto;\n  padding: 30px;\n  width: 60%;\n  display: -webkit-flex;\n  display: flex;\n  -webkit-flex-direction: column;\n          flex-direction: column;\n  -webkit-justify-content: center;\n          justify-content: center;\n  -webkit-align-items: center;\n          align-items: center;\n  font-weight: bold;\n  font-size: 15px;\n  border: 1px solid gray;\n  background-image: linear-gradient(to top, rgba(0,0,0,.5), rgba(0,0,0,.5));\n  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);\n}\n\n.log-input {\n  width: 65%;\n  height: 35px;\n  margin: 0.8em auto;\n  font-weight: bold;\n  border-radius: 4px;\n  border: 1px solid gray;\n}\n\nbutton:hover {\n  background-color: white;\n  border: 1px solid #28a745;\n  color: #28a745;\n}\n", ""]);
+
+// exports
+
+
+/***/ })
+
+})
+//# sourceMappingURL=main.cf2bf6f67327549f3ad5.hot-update.js.map
